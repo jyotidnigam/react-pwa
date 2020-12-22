fca777eea651279b4ba8307a6d0dedb82b76e047
jyotidnigam <jyotinigam135@gmail.com>
2020-12-22 22:12:54 +0530

    Game Page

---
+++ src/components/GameTable/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect,lazy, Suspense } from 'react';
+import { Button, Card, Container, Row, Col, Image  } from 'react-bootstrap';
+
+export const GameTable = () => {
+  return (
+    <div className="background-section">
+      <Container>
+          Table
+      </Container>
+    </div>
+  );
+}
+
+export default GameTable;
