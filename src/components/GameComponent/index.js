27e2b39fb7affdfcb407feecc09844e80ca576ba
jyotidnigam <jyotinigam135@gmail.com>
2020-12-28 23:39:04 +0530

    patch

--- src/components/GameComponent/index.js
+++ src/components/GameComponent/index.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect,lazy, Suspense } from 'react';
-import { Button, Card, Container, Row, Col, Image  } from 'react-bootstrap';
+import { Card, Container, Row, Col, Image  } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import './index.scss';
 import { useGameState } from '../../Context';
-import { gameInitialState } from '../../Context/Reducers/gameReducer';
-import StartScreen from '../StartScreen'
 import { getGameById } from '../../Context/Actions/gameActions';
 
 const VideoPlayer = lazy(() => import('../common/videoPlayer'));
@@ -17,42 +15,26 @@ export const Home = () => {
   const { pathname } = useLocation();
   const gameId = pathname.split('/')[2]
   const { games : { games, game } = {}, gamesDispatch } = useGameState();
-  const [ gameDetails, setGameDetails] = useState();
+  const [ gameDetails, setGameDetails] = useState(null);
 
   useEffect(async()=>{
-    // document.addEventListener('contextmenu', (e) => {
-    //   e.preventDefault();
-    // });
-    const res = await getGameById(gamesDispatch, gameId);
-    debugger
-    setGameDetails(JSON.parse(res.gameJson || ''))
-    
-    gameDetails && start();
-    // const detail = games.length ? games.find((g) => g.gameSlug === gameId) : ''
-    // if(!detail){
-    //   const res = await getGameById(gamesDispatch, gameId);
-    //   if(res){
-    //     // if(Object.keys(game).length)
-    //       setGameDetails(JSON.parse(res.gameJson))
-    //   }else alert(res.error);
-    // }ss  
-    // else
-    //   setGameDetails(JSON.parse(detail.gameJson))
-  }, [game])
+     document.addEventListener('contextmenu', (e) => { 
+       e.preventDefault();
+       });
+     
+      if(!gameDetails){ 
+        const res = await getGameById(gamesDispatch, gameId); 
+        setGameDetails(JSON.parse(res.gameJson || '')) 
+      } 
+      gameDetails && !isStarted && start(); 
+    }, [gameDetails]) 
 
   const start = () => {
-    debugger
-    setIsEnded(false);
     const rootScene = gameDetails['config'].rootScene;
     const scene = gameDetails.scenes[rootScene]
     setCurrentScene(scene);
-    setIsStarted(true);
   }
 
-  const playAgain = () => {
-    setIsEnded(true);
-    setIsStarted(false);
-  }
   const choiceSelection = (followupScene) => { 
     const scene = gameDetails.scenes[followupScene]
     setCurrentScene(scene);
@@ -62,25 +44,23 @@ export const Home = () => {
     <div className="background-section">
      
     <Container>
-      {!isStarted && 
-            <StartScreen start={start}/>
-      }
       {
-        isStarted && !isEnded &&  <Row className="game-dashboard">
+        <Row className="game-dashboard">
           <Col xs={12} className="w-100">
           <div>
             <Card className="bg-dark text-white" id="videoDiv">
-                  { currentScene.image && !currentScene.video && <Card.Img src={'/images/'+currentScene.image}/> }
+                  { currentScene.image && <Card.Img src={'/images/'+currentScene.image}/> }
+                  {
+                    currentScene.movie && <Suspense fallback={()=><div></div>}>
+                     <VideoPlayer url={'/video/'+currentScene.movie} repeat={currentScene.repeatVideo}/>
+                    </Suspense>
+                   }
                   {
                     currentScene.sound && <Suspense fallback={()=><div></div>}>
                         <Music url={'/music/'+currentScene.sound} />
                       </Suspense>
                   }
-                  {
-                    currentScene.video && <Suspense fallback={()=><div></div>}>
-                     <VideoPlayer url={'/video/'+currentScene.video} repeat={currentScene.repeatVideo}/>
-                    </Suspense>
-                   }
+                  
               <Card.ImgOverlay>
                 <div id="videoMessage">
                   <Row>
@@ -90,17 +70,12 @@ export const Home = () => {
                   </Row>
                   <Row className="choices-block">
                   {
-                  currentScene && currentScene.choices.length ? currentScene.choices.map((choice, i) => {
+                  currentScene && currentScene.choices && currentScene.choices.length && currentScene.choices.map((choice, i) => {
                     return <Col xs={12} className="dialog-block mb-2"
                       onClick={() => choiceSelection(choice.followupScene)}>
                         <h6>{choice.text}</h6>                      
                     </Col>
-                  }) :
-                  <Row className="w-100 text-center">
-                    <Col xs={12}>
-                      <Button className="play-btn" onClick={playAgain}>Play Again</Button>
-                  </Col>
-                </Row> 
+                  }) 
                 }
                 </Row>
                 </div>
